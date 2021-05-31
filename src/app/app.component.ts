d229a82fe1613acac10b816ddf0717c373b1b916
Dipen Patel <pateldipen_92@live.com>
2021-05-31 10:06:00 +0530

    New design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,19 +20,25 @@ export class AppComponent {
   isShow = true;
 
   priceList: PriceList[] = [
-    { index: 1, name: 'GI Pipe', price: '99' },
-    { index: 2, name: 'MS Pipe', price: '83' },
-    { index: 3, name: 'Angle', price: '59' },
-    { index: 4, name: 'Channel', price: '61' },
-    { index: 5, name: 'Choras/Patti', price: '57.50' },
-    { index: 6, name: 'Coating Patra', price: '160 Running Feet' },
-    { index: 7, name: 'MS Sheet', price: '82' },
-    { index: 8, name: 'GI Sheet', price: '98' },
-    { index: 9, name: 'Cement Patra', price: '170 Running Meter' },
-    { index: 10, name: 'Shutter Patti', price: '105' },
-    { index: 11, name: 'Guide/Bottom', price: '82' },
-    { index: 12, name: 'Bright Choras/Round', price: '60' },
-    { index: 13, name: 'Bright Patti', price: '62.5' },
+    { index: 1, name: 'GI Pipe Apollo', price: '110/kg' },
+    { index: 2, name: 'MS Pipe', price: 'Coil 89/kg Patti 78/kg' },
+    { index: 3, name: 'Angle 35x5/25x3/30x3', price: '62/kg' },
+    { index: 4, name: 'Channel 3x1.5', price: '63.5/kg' },
+    { index: 5, name: 'Choras/Patti 10mm/25x6/20x5', price: '61/kg' },
+    { index: 6, name: 'JSW Patra without Guardfilm', price: '160 Running Feet' },
+    { index: 7, name: 'MS Sheet 10/12 Guage', price: '86/kg' },
+    { index: 8, name: 'GI Sheet 22/20/18/16 Guage', price: '103/kg' },
+    { index: 9, name: 'Cement Patra Per Piece', price: '6.5ft - 350 / 8ft - 440' },
+    { index: 10, name: 'Cement Patra Per Piece', price: '10ft - 525 / 12ft - 720' },
+    { index: 11, name: 'Shutter Patti Coating', price: '120/kg' },
+    { index: 12, name: 'Guide/Bottom/Braket', price: '84/kg' },
+    { index: 13, name: 'Bright Choras/Round', price: '64.50/kg' },
+    { index: 14, name: 'Bright Patti 25x6/30x6/20x5', price: '67/kg' },
+    { index: 15, name: 'Coating Role/GI Role', price: '120/kg' },
+    { index: 16, name: 'Polycarbonate Sheet', price: '55/sq.ft' },
+    { index: 17, name: 'FRP Sheet', price: '55/sq.ft' },
+    { index: 18, name: 'PVC Sheet', price: '30/sq.ft' },
+    { index: 19, name: 'Gate Channel', price: '69/kg' },
   ];
 
   ngOnInit() {
@@ -102,7 +108,6 @@ export class AppComponent {
       index++;
     });
   }
-
 }
 
 
